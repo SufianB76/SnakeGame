24b7dcb0f05411f02931f8675c2fc99156ee85b2
Sufian <98504017+SufianB76@users.noreply.github.com>
2023-07-12 19:37:40 -0700

    Add files via upload

---
+++ SnakeGame/src/GameFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+public class GameFrame extends JFrame {
+    GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable((false));
+        /* takes all the components and fits them */
+        this.pack();
+        this.setVisible(true);
+        /*makes the window appear in the center of your screen */
+        this.setLocationRelativeTo(null);
+    }
+
+}
+
---
+++ SnakeGame/src/SnakeGame.java
@@ -0,0 +1,6 @@
+public class SnakeGame {
+    public static void main(String[] args) {
+        /*Creating a instance for gameframe class */
+        new GameFrame();
+    }
+}
